94f76f53f2201ceaf9f92caa3bafae0c22424091
Yuan Gao <gaoyua@google.com>
2023-08-11 23:55:15 +0000

    update animation and samples

--- helper.js
+++ helper.js
@@ -1,5 +1,5 @@
 function createAudioHTML(path) {
-  return '<audio controls controlslist="nodownload" class="px-1"> <source src=' +
+  return '<audio controls controlslist="noplaybackrate nodownload" class="px-1"> <source src=' +
       path +
       ' type="audio/wav">Your browser does not support the audio element.</audio>';
 }
@@ -11,6 +11,11 @@ function generateExampleRow(table_row, base_path, filename_ext, col_offset, dire
     } else {
       current_files = [filename_ext[i]];
     }
+    var return_func = (cell, req) => (() =>  {
+        if (req.readyState === req.DONE) {
+          cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
+        };
+      });
     for (var j = 0; j < current_files.length; j++) {
       let cell = table_row.cells[col_offset + i];
       let p = base_path + current_files[j];
@@ -18,26 +23,10 @@ function generateExampleRow(table_row, base_path, filename_ext, col_offset, dire
         cell.innerHTML = direct_text;
         continue
       }
-      // if  (p.endsWith('html')) {
-      //   var req = new XMLHttpRequest();
-      //   req.onreadystatechange = () => {
-      //     if (this.readyState === this.DONE) {
-      //       cell.innerHTML = req.responseText;
-      //     }
-      //   };
-      //   req.open('GET', p, false);
-      //   req.send(null);
-      //   // cell.innerHTML =  '<script src="'+p+'"></script>'
-      //   // continue
-      // }
       if (p.endsWith('txt')) {
         var req = new XMLHttpRequest();
-        req.onreadystatechange = () => {
-          if (this.readyState === this.DONE) {
-            cell.innerHTML = '<font size="-1">' + req.responseText + '</font>';
-          }
-        };
-        req.open('GET', p, false);
+        req.onreadystatechange = return_func(cell, req);
+        req.open('GET', p);
         req.send(null);
       } else {
         cell.innerHTML = cell.innerHTML + createAudioHTML(p);
@@ -51,20 +40,20 @@ function generateImpaintTable(tableId) {
   let table = document.getElementById(tableId);
   let ext = ['.txt', '_gt.wav', '_fast.wav', '_medium.wav', '_slow.wav'];
   let filenames = [
-    'impaint_00',
-    'impaint_01',
+    // 'impaint_00',
+    // 'impaint_01',
     'impaint_02',
     'impaint_03',
     'impaint_04',
-    // 'impaint_05',
+    'impaint_05',
     // 'impaint_06',
-    // 'impaint_07',
-    // 'impaint_08',
-    // 'impaint_09',
+    'impaint_07',
+    'impaint_08',
+    'impaint_09',
     // 'impaint_10',
     // 'impaint_11',
     // 'impaint_12',
-    // 'impaint_13',
+    'impaint_13',
     // 'impaint_14',
     // 'impaint_15',
   ];
@@ -81,22 +70,22 @@ function generatePromptTable(tableId) {
     'prompt_01',
     'prompt_02',
     'prompt_03',
-    'prompt_04',
-    // 'prompt_05',
-    // 'prompt_06',
-    // 'prompt_07',
-    // 'prompt_08',
+    // 'prompt_04',
+    'prompt_05',
+    'prompt_06',
+    'prompt_07',
+    'prompt_08',
     // 'prompt_09',
-    // 'prompt_10',
+    'prompt_10',
     // 'prompt_11',
     // 'prompt_12',
-    // 'prompt_13',
+    'prompt_13',
     // 'prompt_14',
     // 'prompt_15',
-    // 'prompt_16',
-    // 'prompt_17',
+    'prompt_16',
+    'prompt_17',
     // 'prompt_18',
-    // 'prompt_19',
+    'prompt_19',
   ];
   for (var i = 0; i < filenames.length; i++) {
     generateExampleRow(table.rows[1 + i], 'data/' + filenames[i], ext, 0);
@@ -133,25 +122,10 @@ function generateSamplesTable(tableId) {
   }
 }
 
-function generateWavOnlyTables(tableId) {
+function generateSpeakerTables(tableId, filenames, ext=['.txt','_gt.wav', '_0.wav','_1.wav','_2.wav','_3.wav']) {
   let table = document.getElementById(tableId);
-  let ext = [['_0.wav', '_1.wav','_2.wav','_3.wav','_4.wav']];
-  let filenames = [
-    'ioo_007_02923'
-  ];
   for (var i = 0; i < filenames.length; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/' + filenames[i], ext, 1);
-  }
-}
-
-function generateSpeakerTables(tableId) {
-  let table = document.getElementById(tableId);
-  let ext = ['.txt','_gt.wav', '_0.wav','_1.wav','_2.wav','_3.wav'];
-  let filenames = [
-    'example_4',
-  ];
-  for (var i = 0; i < filenames.length; i++) {
-    generateExampleRow(table.rows[1 + i], 'data/' + filenames[i], ext, 0);
+    generateExampleRow(table.rows[1 + i], filenames[i], ext, 0);
   }
 }
 
@@ -159,17 +133,7 @@ function generateSpeakerTables(tableId) {
 generateSamplesTable('samples-table');
 generatePromptTable('prompt-table');
 generateImpaintTable('impaint-table');
-generateWavOnlyTables('single-table');
-generateSpeakerTables('speaker-table');
-
-// cc = document.getElementById('example_0');
-// console.log(cc)
-// var req = new XMLHttpRequest();
-// req.onreadystatechange = () => {
-//   if (this.readyState === this.DONE) {
-//     cc.innerHTML = req.responseText;
-//   }
-// };
-// req.open('GET', 'data/second_example_2.html');
-// req.send(null);
-
+generateSpeakerTables('single-table', ["data/blue_ioo_007_02923"], ext=['_gt.wav', '_0.wav','_1.wav','_2.wav','_3.wav']);
+generateSpeakerTables('speaker-table-02', ["data_anim/blue_mean_example_new_2"]);
+generateSpeakerTables('speaker-table-04', ["data_anim/blue_mean_example_4"]);
+generateSpeakerTables('speaker-table-05', ["data_anim/blue_mean_example_5"]);
